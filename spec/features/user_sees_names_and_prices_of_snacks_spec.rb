4ec902a3d4646cc46443905870996022b1413e8a
Leah Miller <lkmiller.87@gmail.com>
2018-12-21 10:41:03 -0700

    Adds completion of User Story 1

--- spec/features/user_sees_names_and_prices_of_snacks_spec.rb
+++ spec/features/user_sees_names_and_prices_of_snacks_spec.rb
@@ -5,7 +5,7 @@ describe  'vending machine show page' do
     sam = Owner.create(name: "Sam's Snacks")
     machine_1 = sam.machines.create(location: "Turing Basement")
     snack_1 = Snack.create(machines: [machine_1], snack_name: "Milky Way", price: 1)
-    snack_1 = Snack.create(machines: [machine_1], snack_name: "Twix", price: 2)
+    snack_2 = Snack.create(machines: [machine_1], snack_name: "Twix", price: 2)
     visit machine_path(machine_1)
 
     expect(page).to have_content(snack_1.snack_name)
