45870f2f36ce54a056585c4c9deba03eaa62f3a1
Leah Miller <lkmiller.87@gmail.com>
2018-12-21 10:58:26 -0700

    Adds passing test for viewing location

--- spec/features/user_sees_snack_show_page_spec.rb
+++ spec/features/user_sees_snack_show_page_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 describe 'snack show page' do
   describe 'as a visitor' do
-    it "shoiuld see snack names, prices, locations, average prices, and unique itesm count" do
+    it "should see snack names, prices, locations, average prices, and unique items count" do
       sam = Owner.create(name: "Sam's Snacks")
       machine_1 = sam.machines.create(location: "Turing Basement")
       machine_2 = sam.machines.create(location: "Turing Rooftop")
-      snack_1 = Snack.create(machines: [machine_1], snack_name: "Milky Way", price: 1)
+      snack_1 = Snack.create(machines: [machine_1, machine_2], snack_name: "Milky Way", price: 1)
 
       visit snack_path(snack_1)
 
       expect(page).to have_content(snack_1.snack_name)
       expect(page).to have_content(snack_1.price)
+
+      expect(page).to have_content(machine_1.location)
+      expect(page).to have_content(machine_2.location)
 
     end
   end
